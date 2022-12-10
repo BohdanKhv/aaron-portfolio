314d61814aee587b9115c28ec224e2086059d603
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-12-10 11:25:45 -0500

    _

--- src/pages/Cat.jsx
+++ src/pages/Cat.jsx
@@ -15,9 +15,9 @@ const Cat = () => {
             </div>
             <div className="container">
                 <div className="content cat-img">
-                    <img src="/assets/cat-1.jpg" alt="cat 1" loading="lazy"/>
-                    <img src="/assets/cat-2.jpg" alt="cat 2" loading="lazy"/>
-                    <img src="/assets/cat-3.jpg" alt="cat 3" loading="lazy"/>
+                    <img src="/assets/cat-1.jpg" alt="cat 1" loading="lazy" style={{ boxShadow: '12px 12px 0 #FBEC5D' }}/>
+                    <img src="/assets/cat-2.jpg" alt="cat 2" loading="lazy" style={{ boxShadow: '12px 12px 0 #93ff9c' }}/>
+                    <img src="/assets/cat-3.jpg" alt="cat 3" loading="lazy" style={{ boxShadow: '12px 12px 0 #ff9c9c' }}/>
                 </div>
             </div>
         </section>
